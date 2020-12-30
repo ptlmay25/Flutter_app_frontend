b30db27d00e8d7c8c14e83381643cc31794763cc
ptlmay25 <73228525+ptlmay25@users.noreply.github.com>
2020-12-30 18:38:59 +0530

    Update getStarted.dart

--- ibiz/lib/authScreen/getStarted.dart
+++ ibiz/lib/authScreen/getStarted.dart
@@ -42,7 +42,7 @@ class _GetStartedState extends State<GetStarted> {
               child: Text(
                 "Get \nStarted",
                 style: TextStyle(
-                  color: Color(0xff515151),
+                  color: Color(0xff151515),
                   fontSize: 34 * SizeConfig.heightMultiplier,
                   fontFamily: "Roboto",
                   fontWeight: FontWeight.w500,
